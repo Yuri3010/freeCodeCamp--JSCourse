01e5e64ce1c9ef69834d01f8cbe49d2c22f3783d
Yuri Calzzani <83224339+Yuri3010@users.noreply.github.com>
2022-09-02 14:11:44 -0300

    Update caesarsCipher.js

--- caesarsCipher.js
+++ caesarsCipher.js
@@ -1,21 +1,20 @@
 function rot13(str) {
-  const alphabet = " ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  let decodedStr = '';
+    const alphabet = " ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let decodedStr = '';
 
-  for (var i = 0; i < str.length; i++){
+    for (var i = 0; i < str.length; i++) {
+        if (alphabet.indexOf(str[i]) == -1) {
+            decodedStr += str[i];
+        } else if (alphabet.indexOf(str[i]) == 0) {
+            decodedStr += ' '
+        } else if (alphabet.indexOf(str[i]) + 13 >= 27) {
+            decodedStr += alphabet[alphabet.indexOf(str[i]) + 13 - 26];
+        } else if (alphabet.indexOf(str[i]) + 13 < 27) {
+            decodedStr += alphabet[alphabet.indexOf(str[i]) + 13];
+        }
 
-    if(alphabet.indexOf(str[i]) == 0) { 
-      decodedStr += ' '
+
     }
-    else if (alphabet.indexOf(str[i]) + 13 >= 26) {
-      decodedStr += alphabet[alphabet.indexOf(str[i]) + 13 - 26];
-      }
-    else if (alphabet.indexOf(str[i]) + 13 < 25) {
-      decodedStr += alphabet[alphabet.indexOf(str[i]) + 13];
-    }
-      
-  }
-  //console.log(decodedStr) 
-  return decodedStr;
+    console.log(decodedStr)
+    return decodedStr;
 }
-rot13("SERR PBQR PNZC");
